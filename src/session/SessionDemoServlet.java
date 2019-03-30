02455f2a38f9447c869432e56506d37222df5b4b
kuangc1987@gmail.com <g87107121>
2019-03-30 22:13:40 +0800

    session

---
+++ src/session/SessionDemoServlet.java
@@ -0,0 +1,34 @@
+package session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "SessionDemoServlet")
+public class SessionDemoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String id = request.getParameter("id");
+
+
+        HttpSession session = request.getSession();
+
+        Integer result = (Integer) session.getAttribute(id);
+        if (result == null) {
+            session.setAttribute(id, 1);
+        } else {
+            session.setAttribute(id, result + 1);
+        }
+
+
+        response.sendRedirect("add_success.jsp");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
