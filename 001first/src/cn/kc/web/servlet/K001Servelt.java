bdec5a0154494a18036c688b737fd155b73e484d
kuangcheng <kuangcheng-iri@360.cn>
2019-07-18 15:22:04 +0800

    javaee

---
+++ 001first/src/cn/kc/web/servlet/K001Servelt.java
@@ -0,0 +1,34 @@
+package cn.kc.web.servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+
+@WebServlet("/demo")
+public class K001Servelt implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Servlet3.0来了.....");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
