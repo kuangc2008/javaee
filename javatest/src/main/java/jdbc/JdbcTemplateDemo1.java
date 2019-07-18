98ea5bb07d4b37c4a40c43135c3a0030423b4423
kuangcheng <kuangcheng-iri@360.cn>
2019-07-18 16:31:26 +0800

    jdbc temp

---
+++ javatest/src/main/java/jdbc/JdbcTemplateDemo1.java
@@ -0,0 +1,20 @@
+package jdbc;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import util.JDBCUtils2;
+
+/**
+ * JdbcTemplate入门
+ */
+public class JdbcTemplateDemo1 {
+
+    public static void main(String[] args) {
+        //1.导入jar包
+        //2.创建JDBCTemplate对象
+        JdbcTemplate template = new JdbcTemplate(JDBCUtils2.getDataSource());
+        //3.调用方法
+        String sql = "update emp2 set dept_id = 5000 where id = ?";
+        int count = template.update(sql, 4);
+        System.out.println(count);
+    }
+}
