a841fb621ef9db412cc9e7c6fdfcb57c7612be5d
kuangcheng <kuangcheng-iri@360.cn>
2019-07-24 07:00:19 +0800

    标注

--- _02_01_mybatis/src/main/java/com/kc/dao/IUserDao.java
+++ _02_01_mybatis/src/main/java/com/kc/dao/IUserDao.java
@@ -1,10 +1,14 @@
 package com.kc.dao;
 
 import com.kc.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface IUserDao {
 
     List<User> findAll();
+
+    @Select("select * from user")
+    List<User> findAll2();
 }
